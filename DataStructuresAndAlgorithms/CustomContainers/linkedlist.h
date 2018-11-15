d68701eb805f95a819801a67de3444a68cd9c0d5
David Warford <redatonedash@gmail.com>
2018-11-15 12:49:24 -0800

    filling out linked list stubs

--- DataStructuresAndAlgorithms/CustomContainers/linkedlist.h
+++ DataStructuresAndAlgorithms/CustomContainers/linkedlist.h
@@ -1,23 +1,97 @@
 #pragma once
+#include <iostream>
 template <typename T>
 struct node {
-	node<T>* next;
+	node(T data) {
+		this->data = data;
+	}
+	node<T>* next = NULL;
 	T data;
 };
 
 template <typename T>
-struct linkedItterator {
-	node<T> startNode;
-	node<T> currentNode;
-	node<T> lastNode;
-	
-};
-
-struct linkedList {
-
-};
+class linkedItterator;
 
 template <typename T>
-T operator*(linkedItterator<T> i) {
-	return i.current.data;
-}
+class linkedList {
+public:
+	linkedList(){
+		start = NULL;
+		end = NULL;
+	}
+	~linkedList() {
+		if (start) {
+			node<T>* n = start;
+			while (n->next) {
+				node<T>* temp = n;
+				n = n->next;
+				delete temp;
+			}
+		}
+	}
+	node<T>* start;
+	node<T>* end;
+	void push_front(const T& data) {
+
+	}
+	void pop_front() {
+
+	}
+	void insert(linkedItterator<T> *i, T data) {
+		if (i->list != this) {
+			throw "attempted to use foreign itterator";
+		}
+		node<T>* n = new node<T>(data);
+		if (start) {
+			if (i->current->next) {
+				n->next = i->current->next;
+			}
+			i->current->next = n;
+		} else {
+			start = n;
+			end = n;
+			i->current = start;
+			std::cout << i->current;
+		}
+	}
+};
+
+
+template <typename T>
+class linkedItterator {
+	node<T>* current;
+public:
+	friend class linkedList<T>;
+	linkedList<T>* list;
+	linkedItterator(linkedList<T>* l) {
+		list = l;
+		current = l->start;
+	}
+	void Start() {
+		current = list->start;
+	}
+	void End() {
+		current = list->end;
+	}
+	linkedList<T>* getList() {
+		return list;
+	}
+	void Forward() {
+		if (!current->next) {
+			throw "Attempted to access a null index";
+		}
+		current = current.next;
+	}
+	T& operator*() {
+		return current->data;
+	}
+	void operator++() {
+		Forward();
+	}
+	bool operator==(linkedItterator<T> i) {
+		return i.current == current; 
+	}
+	bool operator!=(linkedItterator<T> i) {
+		return i.current != current;
+	}
+};
--- DataStructuresAndAlgorithms/CustomContainers/main.cpp
+++ DataStructuresAndAlgorithms/CustomContainers/main.cpp
@@ -4,5 +4,10 @@
 #include "queue.h"
 #include "linkedlist.h"
 int main() {
+	linkedList<const char*> l = linkedList<const char*>();
+	linkedItterator<const char*> i = linkedItterator<const char*>(&l);
+	l.insert(&i,"alpha");
+	std::cout << *i;
+	
 	return 0;
 }
