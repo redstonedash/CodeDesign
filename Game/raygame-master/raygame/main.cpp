9774f2b59ce7e75e110bbe89b41b1cc464e58edf
David Warford <redatonedash@gmail.com>
2018-11-15 17:55:39 -0800

    started on game

--- Game/raygame-master/raygame/main.cpp
+++ Game/raygame-master/raygame/main.cpp
@@ -11,6 +11,30 @@
 
 #include "raylib.h"
 
+struct Player { //Player is handled diferently than every other object in the game
+	Camera camera = { 0 };
+	Player() {
+		camera.position = { 0.0f, 0.0f, 10.0f };
+		camera.target = { 0.0f, 0.0f, 0.0f };
+		camera.up = { 0.0f, 1.0f, 0.0f };
+		camera.fovy = 45.0f;
+		camera.type = CAMERA_PERSPECTIVE;
+	}
+	Vector3 velocity = { 0,0,0 };
+};
+
+void updatePlayer(Player* p) {
+	p->camera.position.x += p->velocity.x*GetFrameTime();
+	p->camera.position.y += p->velocity.y*GetFrameTime();
+	p->camera.position.z += p->velocity.z*GetFrameTime();
+}
+
+void acceleratePlayer(Player* p, Vector3 wishdir) { //TODO: make quakey
+	p->velocity.x += wishdir.x;
+	p->velocity.y += wishdir.y;
+	p->velocity.z += wishdir.z;
+}
+
 int main()
 {
 	// Initialization
@@ -21,15 +45,11 @@ int main()
 	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
 	// Define the camera to look into our 3d world
-	Camera camera = { 0 };
-	camera.position = { 0.0f, 10.0f, 10.0f };
-	camera.target = { 0.0f, 0.0f, 0.0f };
-	camera.up = { 0.0f, 1.0f, 0.0f };
-	camera.fovy = 120.0f;
-	camera.type = CAMERA_PERSPECTIVE;
+	
 
 
 	SetTargetFPS(60);
+	Player player = Player();
 	//--------------------------------------------------------------------------------------
 
 	// Main game loop
@@ -38,6 +58,13 @@ int main()
 		RayHitInfo h = RayHitInfo();
 		// Update
 		//----------------------------------------------------------------------------------
+		Vector3 wishdir = { 
+			(IsKeyDown(KEY_W) - IsKeyDown(KEY_S)),
+			0,
+			(IsKeyDown(KEY_A) - IsKeyDown(KEY_D)),
+		};
+		acceleratePlayer(&player, wishdir);
+		updatePlayer(&player);
 		// TODO: Update your variables here
 		//----------------------------------------------------------------------------------
 
@@ -47,23 +74,11 @@ int main()
 
 		ClearBackground(RAYWHITE);
 
-		BeginMode3D(camera);
+		BeginMode3D(player.camera);
 
-		DrawCube({ -4.0f, 0.0f, 2.0f }, 2.0f, 5.0f, 2.0f, RED);
-		DrawCubeWires({ -4.0f, 0.0f, 2.0f }, 2.0f, 5.0f, 2.0f, GOLD);
-		DrawCubeWires({ -4.0f, 0.0f, -2.0f }, 3.0f, 6.0f, 2.0f, MAROON);
+		DrawCube({ -0.0f, 0.0f, 5.0f }, 5.0f, 5.0f, 5.0f, RED);
 
-		DrawSphere({ -1.0f, 0.0f, -2.0f }, 1.0f, GREEN);
-		DrawSphereWires({ 1.0f, 0.0f, 2.0f }, 2.0f, 16, 16, LIME);
-
-		DrawCylinder({ 4.0f, 0.0f, -2.0f }, 1.0f, 2.0f, 3.0f, 4, SKYBLUE);
-		DrawCylinderWires({ 4.0f, 0.0f, -2.0f }, 1.0f, 2.0f, 3.0f, 4, DARKBLUE);
-		DrawCylinderWires({ 4.5f, -1.0f, 2.0f }, 1.0f, 1.0f, 2.0f, 6, BROWN);
-
-		DrawCylinder({ 1.0f, 0.0f, -4.0f }, 0.0f, 1.5f, 3.0f, 8, GOLD);
-		DrawCylinderWires({ 1.0f, 0.0f, -4.0f }, 0.0f, 1.5f, 3.0f, 8, PINK);
-
-		DrawGrid(10, 1.0f);        // Draw a grid
+		DrawGrid(1000, 1.0f);        // Draw a grid
 
 		EndMode3D();
 
